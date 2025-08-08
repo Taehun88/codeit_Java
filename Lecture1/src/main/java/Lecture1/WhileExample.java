bfea01e5b48937b218608ce7b9d6e635116dd957
unknown <taehun8235@gmail.com>
2025-08-08 15:07:59 +0900

    while Example upload

---
+++ Lecture1/src/main/java/Lecture1/WhileExample.java
@@ -0,0 +1,12 @@
+package Lecture1;
+
+public class WhileExample {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while(count < 4) {
+            System.out.println("count: " + count);
+            count++;
+        }
+    }
+}
